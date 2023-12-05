b12704d93fc44756365e74be310d7c4d02f8dc06
Diego Fernandez <diego.fernandez@emqu.net>
2023-12-05 19:45:08 -0400

    [FEAT]create routes and pages templates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LabComponent } from './pages/lab/lab.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'home'
+  },
+  {
+    path: 'lab',
+    component: LabComponent,
+    title: 'lab'
+  }
+];
---
+++ src/app/pages/lab/lab.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lab',
+  standalone: true,
+  imports: [],
+  templateUrl: './lab.component.html',
+  styleUrl: './lab.component.css'
+})
+export class LabComponent {
+  title = ''
+  tasks = [
+    'hola',
+    'xdnt'
+  ]
+}
