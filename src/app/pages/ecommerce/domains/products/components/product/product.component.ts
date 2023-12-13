83f0a072ea9e96c2fbac05a8226b530f3067a9af
Diego Fernandez <diegoafanador10@gmail.com>
2023-12-12 22:30:37 -0400

    [FEAT]components, input and output

--- src/app/pages/ecommerce/domains/products/components/product/product.component.ts
+++ src/app/pages/ecommerce/domains/products/components/product/product.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-product',
   standalone: true,
   imports: [],
-  templateUrl: './product.component.html',
-  styleUrl: './product.component.css'
+  templateUrl: './product.component.html'
 })
 export class ProductComponent {
+  @Input({required: true}) img : string = ''
+  @Input({required: true}) title : string = ''
+  @Input({required: true}) price : number = 0
+
+  @Output() addedProduct = new EventEmitter()
+
+  imgSrc: string = 'https://picsum.photos/640/640?r=' + Math.random();
+
+  addToCart(){
+    this.addedProduct.emit(this.img)
+  }
 
 }
--- src/app/pages/ecommerce/ecommerce.routes.ts
+++ src/app/pages/ecommerce/ecommerce.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { ListComponent } from './domains/products/pages/list/list.component';
 
 export default [
   {
     path: '',
-    component: HomeComponent,
+    component: ListComponent,
     title: 'Home'
   }
 ] as Routes ;
