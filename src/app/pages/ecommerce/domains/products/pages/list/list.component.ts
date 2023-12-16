d87989a339034f64ae15b6ed0e71a0983cdbd5b4
Diego Fernandez <diegoafanador10@gmail.com>
2023-12-15 20:14:30 -0400

    [FEAT]product component fix and added header

--- src/app/pages/ecommerce/domains/products/pages/list/list.component.ts
+++ src/app/pages/ecommerce/domains/products/pages/list/list.component.ts
@@ -1,14 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ProductComponent } from '../../components/product/product.component';
 import { CommonModule } from '@angular/common';
+import {product} from 'app/models/product.model'
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [ProductComponent, CommonModule],
+  imports: [ProductComponent, CommonModule, HeaderComponent],
   templateUrl: './list.component.html'
 })
 export class ListComponent {
+
+  products = signal<product[]>([])
+
+  constructor(){
+    this.products.set([
+      {
+        id: Date.now(),
+        title: 'product 1',
+        price: 123,
+        img: 'https://picsum.photos/640/640?r=22',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'product 2',
+        price: 1223,
+        img: 'https://picsum.photos/640/640?r=244',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'product 3',
+        price: 13453,
+        img: 'https://picsum.photos/640/640?r=26',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'product 1',
+        price: 123,
+        img: 'https://picsum.photos/640/640?r=22',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'product 2',
+        price: 1223,
+        img: 'https://picsum.photos/640/640?r=244',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'product 3',
+        price: 13453,
+        img: 'https://picsum.photos/640/640?r=26',
+        createdAt: new Date().toISOString()
+      }
+    ])
+  }
 
   addEvent(event: Event){
     console.log(event)
