dc0cd5d407df1b7cd2cdb511b415443cce73c961
Diego Fernandez <diegoafanador10@gmail.com>
2023-12-18 23:23:52 -0400

    [FEAT]added cart

--- src/app/pages/ecommerce/domains/shared/components/header/header.component.ts
+++ src/app/pages/ecommerce/domains/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  hiddeCart = signal(true)
 
+  toggleCart(){
+    this.hiddeCart.update((state) => !state)
+  }
 }
