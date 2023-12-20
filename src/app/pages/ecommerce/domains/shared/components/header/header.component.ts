355f1f1ac472a3847a28c01289e09878bf23b0a4
Diego Fernandez <diegoafanador10@gmail.com>
2023-12-19 22:07:34 -0400

    [FEAT]add products to cart

--- src/app/pages/ecommerce/domains/shared/components/header/header.component.ts
+++ src/app/pages/ecommerce/domains/shared/components/header/header.component.ts
@@ -1,15 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, signal } from '@angular/core';
+import { product } from 'app/models/product.model';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
   hiddeCart = signal(true)
 
+  @Input() cart : product[] = []
+
   toggleCart(){
     this.hiddeCart.update((state) => !state)
+  }
+
+  getTotalPrice() {
+    return this.cart.reduce((total, product) => total + product.price, 0);
   }
 }
